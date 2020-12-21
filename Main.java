948bb485a08ef5993991bba2b435e3582fd95f9a
SOUTH-BUTOVO <75549049+SOUTH-BUTOVO@users.noreply.github.com>
2020-12-21 18:26:12 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+// Буду доделывать эту работу!!!
+
+package ru.geekbrains;
+
+public class Main {
+
+    
+
+    public static void main(String[] args) {
+
+        Worker[] worker = {
+                new Worker("Артём Евдокимов", 20, "Russian Hacker", "russianhacker@java.com", 8404404, 404404404),
+                new Worker("Кирилл Качалов", 30, "Mentor", "mentor@java.com", 85472550, 50000),
+                new Worker("Владимир Наумов", 40, "Instruktor", "instruktor@java.com", 87412520, 40000),
+                new Worker("Вячеслав Савенок", 50, "Coach", "coach@java.com", 84417921, 60000),
+                new Worker("Анна Чапман", 60, "SPY", "spy@java.com", 87654321, 150000),
+        };
+        System.out.println(worker[1]);
+//        Worker worker1 = new Worker("Артём Евдокимов", 20, "Russian Hacker", "russianhacker@java.com", 8404404, 404404404);
+//        Worker worker2 = new Worker("Кирилл Качалов", 30, "Mentor", "mentor@java.com", 85472550, 50000);
+//        Worker worker3 = new Worker("Владимир Наумов", 40, "Instruktor", "instruktor@java.com", 87412520, 40000);
+//        Worker worker4 = new Worker("Вячеслав Савенок", 50, "Coach", "coach@java.com", 84417921, 60000);
+//        Worker worker5 = new Worker("Анна Чапман", 60, "SPY", "spy@java.com", 87654321, 150000);
+
+//        worker1.getAge(1);
+//        worker2.getAge(40);
+//        worker3.getAge(40);
+//        worker4.getAge(40);
+//        worker5.getAge(40);
+
+//        worker.workers();
+//        worker2.workers();
+//        worker3.workers();
+//        worker4.workers();
+//        worker5.workers();
+    }
+}
